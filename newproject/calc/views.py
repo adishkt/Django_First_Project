889ce6dea66ac3b02a0a3bb05cbfea38b4908f5d
Adish K T <161046695+adishkt@users.noreply.github.com>
2026-07-10 15:10:49 +0530

    done addition of two numbers

--- newproject/calc/views.py
+++ newproject/calc/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html',{'name':'navin'})
+
+
+def add(request):
+    val1=request.GET['num1']
+    val2=request.GET['num2']
+    res=int(val1)+int(val2)
+    return render(request,'result.html',{'result':res})
